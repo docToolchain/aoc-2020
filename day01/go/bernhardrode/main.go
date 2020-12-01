c90d1fb3fa85fec6a6778d0b886709ffb1739a64
Bernhard Rode <mail@bernhardrode.de>
2020-12-01 14:35:21 +0100

    feat: add go solution

---
+++ day01/go/bernhardrode/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+// GetInput of file as int
+func GetInput(filename string) ([]int, error) {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sc := bufio.NewScanner(file)
+	sc.Split(bufio.ScanWords)
+	var result []int
+
+	for sc.Scan() {
+		x, err := strconv.Atoi(sc.Text())
+		if err != nil {
+			return result, err
+		}
+		result = append(result, x)
+	}
+
+	return result, sc.Err()
+}
+
+// FindTwo numbers
+func FindTwo(numbers []int, target int) (int, int, error) {
+	result := make(map[int]int)
+
+	for _, v := range numbers {
+		result[target-v] = v
+		if value, ok := result[v]; ok {
+			return value, v, nil
+		}
+	}
+	return 0, 0, errors.New("not_found")
+}
+
+// FindThree numbers
+func FindThree(input []int, target int) (int, int, int, error) {
+	for i := range input[:len(input)-2] {
+		start, end := i+1, len(input)-1
+
+		for start < end {
+			sum := input[i] + input[start] + input[end]
+
+			if sum > target {
+				end--
+			} else if sum < target {
+				start++
+			} else {
+				return input[i], input[start], input[end], nil
+			}
+		}
+	}
+	return 0, 0, 0, errors.New("not_found")
+}
+
+// main function for aoc day01
+func main() {
+	var filename string
+	flag.StringVar(&filename, "f", "example", "Filename to get input from")
+	flag.Parse()
+
+	year := 2020
+
+	input, err := GetInput(filename)
+	if err != nil {
+		return
+	}
+
+	var n1 int
+	var n2 int
+	var n3 int
+
+	start := time.Now()
+	n1, n2, _ = FindTwo(input, year)
+	fmt.Println("FindTwo", time.Since(start), n1*n2)
+
+	start = time.Now()
+	n1, n2, n3, _ = FindThree(input, year)
+	fmt.Println("FindThree", time.Since(start), n1*n2*n3)
+}
