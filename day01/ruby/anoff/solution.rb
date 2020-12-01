21b015cee2c093a936c085b7828939107227e84f
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2020-12-01 22:59:22 +0900

    this wont work for some reasion

--- day01/ruby/anoff/solution.rb
+++ day01/ruby/anoff/solution.rb
@@ -24,18 +24,49 @@ def part1(input)
         return bigVal * sorted[ix]
       end
     }
+    return nil
 end
 
-def part2(massInventory)
-  fuelSum = 0
-  massInventory.each{ |mass|
-    fuel = required_fuel(mass)
-    while fuel > 0 do
-      fuelSum += fuel
-      fuel = required_fuel(fuel)
-    end
-  }
-  fuelSum
+def part2(input)
+  sorted = input
+    .filter { |val| val < 2020 }
+    .sort
+
+  sorted
+    .reverse
+    .each { |bigVal|
+      puts "new val: %d" % bigVal
+      ix1 = 0
+      ix2 = 1
+      sum = bigVal + sorted[ix1] + sorted[ix2]
+      while ix1 < sorted.size do
+        ix2 = 0
+        ix1 += 1
+        while sum < 2020 do
+          ix2 += 1
+          if ix2 == ix1
+            ix2 += 1
+          end
+          puts "ix1: %d, ix2: %d" % [ix1, ix2]
+          sum = bigVal + sorted[ix1] + sorted[ix2]
+          if ix2 == sorted.size-1
+            puts "breaking inner loop"
+            break
+          end
+        end
+        #puts "ix1: %d, ix2: %d" % [ix1, ix2]
+        
+        puts "%d for %d + %d+%d (%d|%d)" % [sum, bigVal, sorted[ix1], sorted[ix2], ix1, ix2]
+        # if ix2 == 0
+        #   puts "breaking outer loop at sum:%d w/ ix2:%d" % [sum, ix2]
+        #   break
+        # end
+        if sum == 2020
+          return bigVal * sorted[ix1] * sorted[ix2]
+        end
+      end
+    }
+    return -1
 end
 
 if caller.length == 0
@@ -43,6 +74,6 @@ if caller.length == 0
 
   expenses = input.split("\n").map(&:to_i)
 
-  puts "Solution for part1: %d" % part1(expenses)
-  #puts "Solution for part2: %d" % part2(massInventory)
+  #puts "Solution for part1: %d" % part1(expenses)
+  puts "Solution for part2: %d" % part2(expenses)
 end
