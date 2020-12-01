5feff063d9189793111709b3bfe824f4c7a5caa3
JeyP91 <julian@arndt-web.net>
2020-12-01 11:11:04 +0000

    JeyP91 solution day 1

--- day00/groovy/JeyP91/solution.groovy
+++ day00/groovy/JeyP91/solution.groovy
@@ -1,5 +1,46 @@
 #!/usr/bin/env groovy
 
-// tag::helper[]
-println "Hello World"
-// end::helper[]
+ArrayList<String> inputNumbersAsString = Arrays.asList(new File('input.txt').text.split("\n"))
+ArrayList<Integer> inputNumbers = new ArrayList<Integer>()
+for(int i = 0; i < inputNumbersAsString.size(); i++)
+{
+   inputNumbers[i] = Integer.parseInt(inputNumbersAsString[i]);
+}
+
+println "\n"
+part1(inputNumbers)
+println "\n"
+part2(inputNumbers)
+
+void part1(ArrayList<Integer> inputNumbers) {
+    for(int i = 0; i < inputNumbers.size(); i++) {
+        for(int j = i + 1; j < inputNumbers.size(); j++) {
+            int sum = inputNumbers[i] + inputNumbers[j]
+            if(sum == 2020) {
+                int solution = inputNumbers[i] * inputNumbers[j]
+                println "Solution Part 1:"
+                println "   Num1: " + inputNumbers[i]
+                println "   Num2: " + inputNumbers[j]
+                println "   Solution: " + solution
+            }
+        }
+    }
+}
+
+void part2(ArrayList<Integer> inputNumbers) {
+    for(int i = 0; i < inputNumbers.size(); i++) {
+        for(int j = i + 1; j < inputNumbers.size(); j++) {
+            for(int k = j + 1; k < inputNumbers.size(); k++) {
+                int sum = inputNumbers[i] + inputNumbers[j] + inputNumbers[k]
+                if(sum == 2020) {
+                    int solution = inputNumbers[i] * inputNumbers[j] * inputNumbers[k]
+                    println "Solution Part 2:"
+                    println "   Num1: " + inputNumbers[i]
+                    println "   Num2: " + inputNumbers[j]
+                    println "   Num3: " + inputNumbers[k]
+                    println "   Solution: " + solution
+                }
+            }
+        }
+    }
+}
