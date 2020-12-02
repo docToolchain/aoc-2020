f99b96ecf613401e6a06043b192d039259de31cb
Waldemar Schäfer <mydev.ws@gmail.com>
2020-12-02 09:43:20 +0000

    feat(day01): improve typscript solution

--- day01/typescript/wschaef/src/solution1.ts
+++ day01/typescript/wschaef/src/solution1.ts
@@ -1,19 +1,21 @@
 import * as fs from 'fs';
-const input = fs.readFileSync('input1.txt','utf8');
+const input = fs.readFileSync('input-short1.txt','utf8');
 
 const lines = input.split('\n');
 
 let valueList = lines
 .map((line: string) => parseInt(line))
 
-let result
-for (let n of valueList){
-    for (let m of valueList){
-        if(n+m===2020){
-            result = n*m;
-            break;
-        }
-    } 
-};
 
+let check = (): number => {
+    for (let n of valueList){
+        for (let m of valueList){
+            if(n+m===2020){
+                return n*m;
+            }
+        } 
+    };
+}
+
+let result = check()
 console.log(result)
