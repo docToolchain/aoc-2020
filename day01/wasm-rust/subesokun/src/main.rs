114f3a5e7bc160130d5b61a1b3272d929176fc5b
subesokun <6920421+subesokun@users.noreply.github.com>
2020-12-02 12:27:21 +0000

    chore: clean up day01 solution

--- day01/wasm-rust/subesokun/src/main.rs
+++ day01/wasm-rust/subesokun/src/main.rs
@@ -20,9 +20,9 @@ fn main() {
     let input_path = Path::new("input.txt");
     let puzzle_input = read_puzzle_input(&input_path);
 
-    // let solution_part_1 = solution::run_star1(puzzle_input);
-    // println!("Solution to part 1: {}", solution_part_1);
+    let solution_part_1 = solution::run_star1(puzzle_input.clone());
+    println!("Solution to part 1: {}", solution_part_1);
 
-    let solution_part_2 = solution::run_star2(puzzle_input);
+    let solution_part_2 = solution::run_star2(puzzle_input.clone());
     println!("Solution to part 2: {}", solution_part_2);
 }
