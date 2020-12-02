ff248c65089137773431a5c19c5723c386f0a7e6
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2020-12-02 13:34:39 +0000

    star2

--- day02/ruby/anoff/solution.rb
+++ day02/ruby/anoff/solution.rb
@@ -8,6 +8,15 @@ def validate_password(password, policy)
   return isCount >= min && isCount <= max
 end
 
+def validate_password2(password, policy)
+  parts = policy.split(" ")
+  policyChar = parts[1]
+  p1, p2 = parts[0].split("-").map(&:to_i)
+
+  isCount = password[p1-1].count(policyChar) + password[p2-1].count(policyChar)
+  return isCount == 1
+end
+
 def part1(input)
   input
     .map { |e|
@@ -17,7 +26,11 @@ def part1(input)
 end
 
 def part2(input)
-
+  input
+    .map { |e|
+      validate_password2(e[:password], e[:policy])
+    }
+    .count(true)
 end
 
 if caller.length == 0
@@ -30,5 +43,5 @@ if caller.length == 0
       { policy: parts[0], password: parts[1] }
     }
   puts "Solution for part1: %d" % part1(entries)
-  #puts "Solution for part2: %d" % part2(entries)
+  puts "Solution for part2: %d" % part2(entries)
 end
--- day02/ruby/anoff/solution_test.rb
+++ day02/ruby/anoff/solution_test.rb
@@ -12,4 +12,15 @@ class TestSolution < Test::Unit::TestCase
       assert_equal valid, validate_password(input[1], input[0]), "Expected #{input[1]} (#{input[0]}) to be #{valid}"
     }
   end
+
+  def test_p1
+    d = {
+      ['1-3 a', 'abcde'] => true,
+      ['1-3 b', 'cdefg'] => false,
+      ['2-9 c', 'ccccccccc'] => false
+    }
+    d.each { |input, valid|
+      assert_equal valid, validate_password2(input[1], input[0]), "Expected #{input[1]} (#{input[0]}) to be #{valid}"
+    }
+  end
 end
