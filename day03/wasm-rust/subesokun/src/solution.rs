2499a67e94138117adf15f082ec2105ef26de4b4
subesokun <6920421+subesokun@users.noreply.github.com>
2020-12-03 20:21:45 +0000

    feat: improve day03 solution

--- day03/wasm-rust/subesokun/src/solution.rs
+++ day03/wasm-rust/subesokun/src/solution.rs
@@ -1,32 +1,42 @@
-fn extract_map(puzzle_input: std::vec::Vec<String>, step_with: usize) -> std::vec::Vec<std::vec::Vec<bool>> {
+//tag::star1[]
+struct Map {
+    map: std::vec::Vec<std::vec::Vec<bool>>,
+    fragment_width: usize,
+    height: usize
+}
+
+impl Map {
+    pub fn get(&self, x: usize, y: usize) -> bool {
+        return self.map[y % self.height][x % self.fragment_width]
+    }
+}
+
+fn generate_map(puzzle_input: std::vec::Vec<String>) -> Map {
+    let height = puzzle_input.len();
     let mut iter = puzzle_input.iter().peekable();
-    let map_height = puzzle_input.len();
-    let map_fragment_width = iter.peek().unwrap().len();
-    let min_map_width = map_height * step_with + 1;
-    let map_fragment_repeat_count = (min_map_width as f64 / map_fragment_width as f64).ceil() as usize;
-    let map_width = map_fragment_repeat_count * map_fragment_width;
-    let mut map = vec![vec![false; map_width]; map_height]; // true means empty
+    let fragment_width = iter.peek().unwrap().len();
+    let mut map = vec![vec![false; fragment_width]; height]; // true means empty
     for (pos_y, line) in iter.enumerate() {
         for (pos_x, grid_value) in line.chars().enumerate() {
             if grid_value == '#' {
-                for repeat_index in 0..map_fragment_repeat_count {
-                    map[pos_y][pos_x + repeat_index * map_fragment_width] = true;
-                }
+                map[pos_y][pos_x] = true;
             }
         }
-
     }
-    return map;
+    return Map {
+        map: map,
+        fragment_width: fragment_width,
+        height: height
+    }
 }
 
-//tag::star1[]
-fn count_encountered_trees(step_x: usize, step_y: usize, map: &std::vec::Vec<std::vec::Vec<bool>>) -> u32 {
+fn count_encountered_trees(step_x: usize, step_y: usize, map: &Map) -> u32 {
     let mut pos_x = 0;
     let mut pos_y = 0;
     let mut encountered_trees = 0;
-    let map_height = map.len();
+    let map_height = map.height;
     while pos_y < map_height {
-        if map[pos_y][pos_x] {
+        if map.get(pos_x, pos_y) {
             encountered_trees += 1;
         }
         pos_x += step_x;
@@ -36,7 +46,7 @@ fn count_encountered_trees(step_x: usize, step_y: usize, map: &std::vec::Vec<std
 }
 
 pub fn run_star1(puzzle_input: std::vec::Vec<String>) -> u32 {
-    let map = extract_map(puzzle_input, 3);
+    let map = generate_map(puzzle_input);
     let solution: u32 = count_encountered_trees(3, 1, &map);
     return solution;
 }
@@ -44,7 +54,7 @@ pub fn run_star1(puzzle_input: std::vec::Vec<String>) -> u32 {
 
 //tag::star2[]
 pub fn run_star2(puzzle_input: std::vec::Vec<String>) -> u32 {
-    let map = extract_map(puzzle_input, 7);
+    let map = generate_map(puzzle_input);
     let mut solution = 1;
     solution *= count_encountered_trees(1, 1, &map);
     solution *= count_encountered_trees(3, 1, &map);
