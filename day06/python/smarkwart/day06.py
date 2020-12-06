2e2b266a7992cf27b4ad5545b500040c58fbda62
Sven Markwart <>
2020-12-06 18:16:53 +0100

    My solution for star2 of day06

--- day06/python/smarkwart/day06.py
+++ day06/python/smarkwart/day06.py
@@ -25,13 +25,14 @@ def create_declarations_list_from_raw_data(raw_declarations):
     Creates a list of declarations where each passwort is a dictionary
     """
     declarations_list = []
-    new_group = ""
+    new_group = {'declarations':"", 'persons':0}
     for line in raw_declarations:
         if line.strip() != '':
-            new_group += line.strip()
+            new_group['declarations'] += line.strip()
+            new_group['persons'] += 1
         else:
             declarations_list.append(new_group)
-            new_group = ""
+            new_group = {'declarations':"", 'persons':0}
 
     #in case data does not end with newline there might be unprocessed password data
     if new_group != "":
@@ -41,12 +42,17 @@ def create_declarations_list_from_raw_data(raw_declarations):
 
 def count_positive_questions(declarations_list):
     """
-    docstring
+    Counts total number of positive answered questions for star1
     """
-    total_count = 0
+    total_count_all_positive = 0
+    total_count_common_positive = 0
     for declaration in declarations_list:
-        total_count += len(remove_duplicate_char(declaration))
-    return total_count
+        string_without_duplicates = remove_duplicate_char(declaration.get('declarations'))
+        total_count_all_positive += len(string_without_duplicates)
+        for char in string_without_duplicates:
+            if declaration.get('declarations').count(char) == declaration.get('persons'):
+                total_count_common_positive += 1
+    return total_count_all_positive, total_count_common_positive
 
 def remove_duplicate_char(input_string):
     """
