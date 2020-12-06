6380e65ca1bcc43340b5ac0a4020dd1621ca54e0
Julian Arndt <julian@arndt-web.net>
2020-12-06 12:25:05 +0100

    Documentation day05

--- day05/groovy/JeyP91/solution.groovy
+++ day05/groovy/JeyP91/solution.groovy
@@ -1,11 +1,15 @@
+// tag::splitInput[]
 ArrayList<String> input = Arrays.asList(new File('input.txt').text.split(System.getProperty("line.separator")))
+// end::splitInput[]
 println("Solution Part 1: " + getMax(input))
 println("Solution Part 2: " + getMissingSeat(input))
 
 int getMax(ArrayList<String> input) {
+    // tag::getMax[]
     return input.collect( seat -> {
         getNumberFromSeatString(seat as String)
     }).max()
+    // end::getMax[]
 }
 
 int getMin(ArrayList<String> input) {
@@ -15,20 +19,26 @@ int getMin(ArrayList<String> input) {
 }
 
 int getNumberFromSeatString(String seat) {
+    // tag::getNumberFromSeatString[]
     return Integer.parseInt(seat.replace("B", "1").replace("F", "0").replace("R", "1").replace("L", "0"), 2)
+    // end::getNumberFromSeatString[]
 }
 
 int getMissingSeat(ArrayList<String> input) {
-    ArrayList transform = input.collect( seat -> {
+    // tag::getMissingSeatTransform[]
+    ArrayList seatList = input.collect( seat -> {
         getNumberFromSeatString(seat as String)
     })
+    // end::getMissingSeatTransform[]
 
+    // tag::getMissingSeat[]
     int missingSeat = 0
     for (int i = getMin(input); i <= getMax(input); i++){
-        if (!transform.contains(i)){
+        if (!seatList.contains(i)){
             missingSeat = i
             break
         }
     }
     return missingSeat
+    // end::getMissingSeat[]
 }
