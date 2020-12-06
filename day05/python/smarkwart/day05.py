007d7e6d0942801320402db8a2d8a22279d352aa
Sven Markwart <>
2020-12-06 14:32:05 +0100

    Update for day05 with simplified solution

--- day05/python/smarkwart/day05.py
+++ day05/python/smarkwart/day05.py
@@ -11,36 +11,25 @@ def get_input_data_as_list(file_name):
         data_list = input_file.readlines()
     return data_list
 
-def transform_binarystring_to_number(binary_string, set_char):
+def transform_binarystring_to_number(binary_string, set_char, transform_lut):
     """
     Transforms the binary string into the row number
-    set_char euqals the char which equals to '1' bit 
+    set_char equals the char which equals to '1' bit 
+    of binary string
     """
+
     row_id = 0
-    for idx, char in enumerate(binary_string[::-1]):
-        if char == set_char:
-            row_id += pow(2,idx)
+    for idx, char in enumerate(binary_string):
+        if char in set_char:
+            row_id += transform_lut[idx]
     return row_id
-
-def get_row_string_from_string(full_string):
-    """
-    Gets the row string from the full string
-    """
-    return full_string[0:7]
-
-def get_column_string_from_string(full_string):
-    """
-    Gets the row string from the full string
-    """
-    return full_string[7:10]
 
 def calculate_seat_id(seat_code):
     """
     calculates seat id from seat code
     """
-    row = transform_binarystring_to_number(get_row_string_from_string(seat_code), "B")
-    column = transform_binarystring_to_number(get_column_string_from_string(seat_code), "R")
-    return row * 8 + column
+    transform_lut = [pow(2,value) for value in range(9, -1, -1)]
+    return transform_binarystring_to_number(seat_code, ['B', 'R'], transform_lut)
 
 def get_seat_codes(seat_list):
     """
@@ -52,11 +41,7 @@ def get_my_seat(seat_codes):
     seat_codes.sort()
     return set(range(seat_codes[0], seat_codes[-1] + 1)).difference(seat_codes)
 
-
-
 seat_list = get_input_data_as_list(sys.argv[1])
 seat_codes = get_seat_codes(seat_list)
-
 print(f"Highest SeatId: {max(seat_codes)}")
 print(f"My seat is: {get_my_seat(seat_codes)}")
-
