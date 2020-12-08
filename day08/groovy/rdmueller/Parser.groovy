933abaa60fac6a7b8e7dc5fb0f50a2cbfab3fcda
Ralf D. Müller <ralf.d.mueller@gmail.com>
2020-12-08 09:03:00 +0000

    day08

---
+++ day08/groovy/rdmueller/Parser.groovy
@@ -0,0 +1,12 @@
+
+class Parser {
+
+    static void parse(String opcodes) {
+        Vm.program = []
+        Vm.state = [pointer:0, acc:0]
+        opcodes.eachLine { line ->
+            line = line.split(" ")
+            Vm.program << [op:line[0],p1:[line[1] as Integer]]
+        }
+    }
+}
