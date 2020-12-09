b003715b8f12388b99f991fef1ac01541e8104a5
t-pi <ti.pi@online.de>
2020-12-09 16:50:23 +0000

    t-pi-day09 star2 @python

--- day09/python/t-pi/solution.py
+++ day09/python/t-pi/solution.py
@@ -1,6 +1,6 @@
 import math
 
-trackback = 5
+trackback = 25
 
 def read_daily_input(filename):
     ''' Read lines from file with given input name
@@ -11,6 +11,8 @@ def read_daily_input(filename):
         return return_list
 
 def check_code(code, codes_sublist):
+    ''' Boolean check if any two number in sublist sum up to code
+    '''
     for line, num1 in enumerate(codes_sublist):
         diff = code - num1
         for num2 in codes_sublist[line+1:]:
@@ -19,6 +21,8 @@ def check_code(code, codes_sublist):
 
 
 def get_next_XMAS_error(codes_list):
+    ''' Runs through codes_list with sliding window to check XMAS consistence
+    '''
     cursor = trackback
     while (cursor < len(codes_list)):
         code = codes_list[cursor]
@@ -27,33 +31,58 @@ def get_next_XMAS_error(codes_list):
         cursor += 1
     return 0
 
-def filter_codelist(codes_list, max):
-    return_list = [item for item in codes_list if (item < max)]
+def reduce_codelist(codes_list, max):
+    ''' Removes any values > max from codes_list
+    '''
+    return_list = [item for item in codes_list if (item <= max)]
     return return_list
 
-def get_codebreaking_list(goal, codes_list, summands = set()):
-    reduced_list = filter_codelist(codes_list, goal)
+def get_codebreaking_list_non_contigous(goal, codes_list, summands = set()):
+    ''' AMBIGUOUS RESULTS! Needed to look up contiguous...
+        Recursively tests for any summands in codes_list, whether they sum up to goal
+        Returns set of summands
+    '''
+    reduced_list = reduce_codelist(codes_list, goal)
     if (reduced_list == []): return None
     print(reduced_list, summands)
     for line, num in enumerate(reduced_list):
         new_goal = goal - num
         if (new_goal < 0): 
-                continue
+                return None
         summands.add(num)
         if (new_goal == 0): 
                 return summands
-        new_summands = get_codebreaking_list(new_goal, reduced_list, summands)
-        if (not new_summands):
+        new_summands = get_codebreaking_list(new_goal, reduced_list[line + 1:], summands)
+        print("new:", new_summands)
+        if (new_summands == None):
             summands.discard(num)
         else:
             summands.update(new_summands)
     return summands
 
+def get_contiguous_codebreaking_list(goal, codes_list):
+    ''' Runs through reduced list with sliding window to find contiguous numbers summing up to goal.
+        Window is increased until either sum is found or sum > goal
+        Returns sum's window.
+    '''
+    reduced_list = reduce_codelist(codes_list, goal)
+    for line, num in enumerate(reduced_list):
+        sum = 0
+        cursor = line
+        while ((sum < goal) and (cursor < len(reduced_list))):
+            sum += reduced_list[cursor]
+            cursor += 1
+            if (sum == goal):
+                return reduced_list[line:cursor]
+            if (sum > goal):
+                break
+    return []
+        
 def main():
-    daily_list = read_daily_input('input_test.txt')
+    daily_list = read_daily_input('input09.txt')
     star1 = get_next_XMAS_error(daily_list)
     print(f"First XMAS code error at: {star1}")
-    star2_list = get_codebreaking_list(star1, daily_list)
+    star2_list = get_contiguous_codebreaking_list(star1, daily_list)
     print(star2_list)
     star2 = min(star2_list) + max(star2_list)
     print(f"Breaking pair's sum: {star2}")
