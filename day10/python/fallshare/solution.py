7262700cc3cf680f2ae31d01483ec33a03f545b0
fallshare <fallscheer@googlemail.com>
2020-12-11 14:40:08 +0000

    Day10 Star 2 solved.\n Clean up required

--- day10/python/fallshare/solution.py
+++ day10/python/fallshare/solution.py
@@ -17,7 +17,7 @@ def add_device(data):
 def get_histogram(data):
     histogram = [1, 0, 0]
 
-    for i in range(1, len(data)):
+    for i in range(1, len(data)): 
         diff = data[i] - data[i - 1] - 1
         histogram[diff] += 1
 
@@ -30,13 +30,50 @@ def get_possible_combinatios(data):
     return
 
 
+#break down list into smaller lists
+
+def breakdown_data(data):
+    # search for all elements where jolt distant is three
+    print(data)
+    adapters_with_max_distance = list()
+    total_combinations = 1
+
+    for i in range(1, len(data)):
+        diff = data[i] - data[i - 1]         
+        if diff == 3:
+            adapters_with_max_distance.append(i - 1)
+    
+    lower_bound = 0
+    for j in range(1, len(adapters_with_max_distance)):
+        
+        upper_bound = adapters_with_max_distance[j]
+        sub_list = data[lower_bound : upper_bound + 1]
+        lower_bound = upper_bound + 1
+        print(sub_list)
+        global combinations
+        combinations = 0
+
+        recurse_through_data(sub_list,0,0)
+
+        total_combinations *= combinations
+        print(combinations)
+    print(total_combinations)
+    return adapters_with_max_distance
+
+
 
 def recurse_through_data(data,index,depth):
     global combinations
+
+    if len(data) == 1:
+        combinations = 1
+        return    
+    
     for i in range(1,4):
         #abort if last element is found
-        print(f"depth:{depth} - i: {i}")
-        if data[index + i] == max(data):
+        diff = data[index + i] - data[index]
+
+        if (data[index + i] == max(data)) and (diff <= 3):
             print(f"i:{i} index: {index} depth:{depth} {max(data)}")
             combinations += 1
             return 
@@ -44,9 +81,9 @@ def recurse_through_data(data,index,depth):
         if (index + i) >= len(data):
             return 
 
-        diff = data[index + i] - data[index]
+        
         if diff <= 3:
-            print(f"{data[index + i]}", end = " ")
+            #print(f"{data[index + i]}", end = " ")
             recurse_through_data(data, index + i, depth + 1)
         else:
             #abort since next elemnt is not matching         
@@ -72,6 +109,6 @@ if __name__ == "__main__":
     adapter_2.insert(0,0)
     global combinations 
     combinations = 0
-    get_possible_combinatios(adapter_2)
-    print(combinations)
-
+    #get_possible_combinatios(adapter_2)
+    #print(combinations)
+    breakdown_data(adapter_2)
