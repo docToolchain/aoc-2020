84a2bd2f4163b644fb8a4867b71b787f6ccc902f
fallshare <fallscheer@googlemail.com>
2020-12-11 15:24:30 +0000

    Day10 done

--- day10/python/fallshare/solution.py
+++ day10/python/fallshare/solution.py
@@ -15,7 +15,7 @@ def add_device(data):
     return data
 
 def get_histogram(data):
-    histogram = [1, 0, 0]
+    histogram = [0, 0, 0]
 
     for i in range(1, len(data)): 
         diff = data[i] - data[i - 1] - 1
@@ -23,42 +23,42 @@ def get_histogram(data):
 
     return histogram
 
-def get_possible_combinatios(data):
-   
-    recurse_through_data(data, 0, 0)
 
-    return
-
-
-#break down list into smaller lists
 
 def breakdown_data(data):
-    # search for all elements where jolt distant is three
-    print(data)
+    # all adapters in the box where the next adapter has a jolt distance of 3 
+    # must be in the adapter chain otherwhise no chain can be formed
+    # thoes adapters can be seen as seperators that can be used to split down the big bag of adapters
+    # into smaller list of adapters
+    # the possible combinations for those smaller list can  individually be calculated
+    # in the end the amout of combinations of the sublist is multiplied to get the  total number of possible combinations
+    
+
     adapters_with_max_distance = list()
     total_combinations = 1
 
+    # find all adapters that act as seperators
     for i in range(1, len(data)):
         diff = data[i] - data[i - 1]         
         if diff == 3:
             adapters_with_max_distance.append(i - 1)
     
+    # split down list of adapters into sub list and calculate posible combinations per list
     lower_bound = 0
     for j in range(1, len(adapters_with_max_distance)):
-        
         upper_bound = adapters_with_max_distance[j]
+
         sub_list = data[lower_bound : upper_bound + 1]
         lower_bound = upper_bound + 1
-        print(sub_list)
+        
         global combinations
         combinations = 0
-
+        #use a recursive function to collect all possible combinations in the sub list
         recurse_through_data(sub_list,0,0)
 
         total_combinations *= combinations
-        print(combinations)
-    print(total_combinations)
-    return adapters_with_max_distance
+    
+    return total_combinations
 
 
 
@@ -74,13 +74,13 @@ def recurse_through_data(data,index,depth):
         diff = data[index + i] - data[index]
 
         if (data[index + i] == max(data)) and (diff <= 3):
-            print(f"i:{i} index: {index} depth:{depth} {max(data)}")
+            #print(f"i:{i} index: {index} depth:{depth} {max(data)}")
             combinations += 1
             return 
+
         #abort if end is found
         if (index + i) >= len(data):
             return 
-
         
         if diff <= 3:
             #print(f"{data[index + i]}", end = " ")
@@ -98,17 +98,17 @@ if __name__ == "__main__":
 
     adapters = read_input_file("input.txt")
     adapters = add_device(adapters)
+    #add source
+    adapters.insert(0,0)
     histogram = get_histogram(adapters)
     
     print(f"Star 1: Solution {histogram[0] * histogram[2]}")
 
 
-    adapter_2 = read_input_file("input.txt")
-    adapter_2 = add_device(adapter_2)
+    adapter = read_input_file("input.txt")
+    adapter = add_device(adapter)
     #add source
-    adapter_2.insert(0,0)
-    global combinations 
-    combinations = 0
-    #get_possible_combinatios(adapter_2)
-    #print(combinations)
-    breakdown_data(adapter_2)
+    adapter.insert(0,0)
+ 
+    print(f"Star 2: Solution {breakdown_data(adapter)}")
+    
