7e5e029d63ad9793e886ddc2d72d6f3a34868f65
jamhocken <hockenberry@ribice.de>
2020-12-12 02:01:28 +0100

    Fixed Day 10 solution in Python and extended docu

--- day10/python/jamhocken/solution.py
+++ day10/python/jamhocken/solution.py
@@ -4,14 +4,17 @@ def process_input(file_contents):
     return [int(line) for line in file_contents]
 
 def find_combinations(width):
-    if width <= 0:
-        return 0
-    if width == 1:
+    if width <= 1:
         return 1
+    elif width == 2:
+        return 2
+    elif width == 3:
+        return 4
     else:
-        return 2*find_combinations(width-1) - find_combinations(width-3)
+        return 2*find_combinations(width-1) - find_combinations(width-4)
+#       return find_combinations(width-1) + find_combinations(width-2) + find_combinations(width-3)
 
-with open("adapters.txt",'r') as adapters_file: 
+with open("adapters.txt",'r') as adapters_file:
     all_adapters_file = adapters_file.readlines()
 
 adapters = process_input(all_adapters_file)
@@ -28,7 +31,6 @@ print('Their product is', diff.count(1)*diff.count(3))
 depth = 0
 count = list()
 while depth < len(diff):
-    depth2 = depth
     depth2 = diff.index(3,depth)
     if depth2-depth < 2:
         count.append(1)
@@ -51,7 +53,7 @@ def alt_find_combinations(list_adapters):
             if next_adapter in list_adapters:
                 sums += adapter_dictionary[next_adapter]
         adapter_dictionary[adapter] = sums
-
+    print(adapter_dictionary)
     return adapter_dictionary[max(list_adapters)]
 
 print('The total number of distinct ways to arrange the adapters to connect\
