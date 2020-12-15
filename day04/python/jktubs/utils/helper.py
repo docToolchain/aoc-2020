a16ada9b671246c4e34ff1f09f63d962feb56c77
LAPTOP-8QAL29T1\jensk <jenskramer@gmx.com>
2020-12-15 14:42:14 +0100

    simplified regex

--- day04/python/jktubs/utils/helper.py
+++ day04/python/jktubs/utils/helper.py
@@ -41,7 +41,7 @@ def check_passport_content(data):
     for c in required_content:
         if c not in data:
             log.debug("invalid: {} is missing in {}".format(c, data))
-            is_valid = False
+            return False
         else:
             # log.debug("okay: {} in {}".format(c, data))
             pass
@@ -86,21 +86,21 @@ def check_passport_extended(data):
 
     if key_error > 0:
         log.debug("key_error: {}".format(key_error))
-        is_valid = False
+        return False
 
     log.debug(passport_fields)
 
     # https://www.w3schools.com/python/python_regex.asp
 
     # "pid": -1,  # pid (Passport ID) - a nine-digit number, including leading zeroes.
-    if re.match(r"[0-9]{9}\b", passport_fields["pid"]):
+    if re.match(r"^[0-9]{9}$", passport_fields["pid"]):
         log.debug('pid valid: {}'.format(passport_fields["pid"]))
     else:
         log.debug('pid invalid: {}'.format(passport_fields["pid"]))
-        is_valid = False
+        return False
 
     # "ecl": "",  # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-    exp = re.compile('(amb|blu|brn|gry|grn|hzl|oth){1}')
+    exp = re.compile('^(amb|blu|brn|gry|grn|hzl|oth){1}$')
     match_list = re.findall(exp, passport_fields["ecl"])
     log.debug("match: {}".format(match_list))
     log.debug(type(match_list))  # ==> list
@@ -109,78 +109,71 @@ def check_passport_extended(data):
     else:
         log.debug("match: {}".format(match_list))
         log.debug('ecl invalid: {}'.format(passport_fields["ecl"]))
-        is_valid = False
+        return False
 
     # "eyr": -1,  # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-    if re.match(r"[0-9]{4}\b", passport_fields["eyr"]):
+    if re.match(r"^[0-9]{4}$", passport_fields["eyr"]):
         log.debug('eyr: {}'.format(passport_fields["eyr"]))
         if 2020 <= int(passport_fields["eyr"]) <= 2030:
             log.debug('eyr valid')
         else:
             log.debug('eyr invalid')
-            is_valid = False
+            return False
     else:
         log.debug('eyr invalid: {}'.format(passport_fields["eyr"]))
-        is_valid = False
+        return False
 
     # "hcl": "",  # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-    if re.match(r"#[a-f0-9]{6}\b", passport_fields["hcl"]):
+    if re.match(r"^#[a-f0-9]{6}$", passport_fields["hcl"]):
         log.debug('hcl valid: {}'.format(passport_fields["hcl"]))
     else:
         log.debug('hcl invalid: {}'.format(passport_fields["hcl"]))
-        is_valid = False
+        return False
 
     # "byr": -1,  # byr (Birth Year) - four digits; at least 1920 and at most 2002.
-    if re.match(r"[0-9]{4}\b", passport_fields["byr"]):
+    if re.match(r"^[0-9]{4}$", passport_fields["byr"]):
         log.debug('byr: {}'.format(passport_fields["byr"]))
         if 1920 <= int(passport_fields["byr"]) <= 2002:
             log.debug('byr valid')
         else:
             log.debug('byr invalid')
-            is_valid = False
+            return False
     else:
         log.debug('byr invalid: {}'.format(passport_fields["byr"]))
-        is_valid = False
+        return False
 
     # "iyr": "",  # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-    if re.match(r"[0-9]{4}\b", passport_fields["iyr"]):
+    if re.match(r"^[0-9]{4}$", passport_fields["iyr"]):
         log.debug('iyr: {}'.format(passport_fields["iyr"]))
         if 2010 <= int(passport_fields["iyr"]) <= 2020:
             log.debug('iyr valid')
         else:
             log.debug('iyr invalid')
-            is_valid = False
+            return False
     else:
         log.debug('iyr invalid: {}'.format(passport_fields["iyr"]))
-        is_valid = False
+        return False
 
     # "hgt": "",  # hgt (Height) - a number followed by either cm or in:
     # # If cm, the number must be at least 150 and at most 193.
     # # If in, the number must be at least 59 and at most 76.
     # "cid": ""  # cid (Country ID) - ignored, missing or not.
-    exp = re.compile('^([0-9]+)(in|cm){1}')
-    match_list = re.findall(exp, passport_fields["hgt"])
-    log.debug("match: {}".format(match_list))
-    log.debug(type(match_list))  # ==> list
-    if len(match_list) == 1:
-        if match_list[0][1] == "cm":
-            if 150 <= int(match_list[0][0]) <= 193:
-                log.debug('{}{} hgt valid'.format(match_list[0][0], match_list[0][1]))
-            else:
-                log.debug('{}{} hgt invalid'.format(match_list[0][0], match_list[0][1]))
-                is_valid = False
-        elif match_list[0][1] == "in":
-            if 59 <= int(match_list[0][0]) <= 76:
-                log.debug('{}{} hgt valid'.format(match_list[0][0], match_list[0][1]))
-            else:
-                log.debug('{}{} hgt invalid'.format(match_list[0][0], match_list[0][1]))
-                is_valid = False
-        else:
-            log.debug('{}{} hgt invalid'.format(match_list[0][0], match_list[0][1]))
-            is_valid = False
+    #exp = re.compile('^([0-9]+)(in|cm){1}$')
+    #match_list = re.findall(exp, passport_fields["hgt"])
+    #log.debug("match: {}".format(match_list))
+    #log.debug(type(match_list))  # ==> list
+
+    m = re.search("^([0-9]+)(in|cm)$", passport_fields['hgt'])
+    log.debug("match: {}".format(m))
+    log.debug(type(m))  # ==> list
+    if not m:
+        return False
+    hgt = int(m.group(1))
+    if m.group(2) == 'cm':
+        if hgt < 150 or hgt > 193:
+            return False
     else:
-        log.debug("match: {}".format(match_list))
-        log.debug('hgt invalid: {}'.format(passport_fields["hgt"]))
-        is_valid = False
+        if hgt < 59 or hgt > 76:
+            return False
 
     return is_valid
