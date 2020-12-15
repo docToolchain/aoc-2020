288ea009f54ee283b61823fa96bf29167b8e44f0
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2020-12-15 14:32:13 +0900

    part1

---
+++ day15/ruby/anoff/solution.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+def part1 (input)
+  store = Hash.new # create hashmap of number->turn it was spoken
+  lastNumber = -1
+  for turn in (0..2020-1) # 2020 - 1, because we use 0 indexing
+    if turn <= (input.size-1)
+      if turn > 0
+        store[lastNumber] = turn - 1
+      end
+      lastNumber = input[turn]
+    else
+      # p "lastNumber: #{lastNumber}, store:#{store[lastNumber]}, turn:#{turn-1}"
+      diff = store[lastNumber].nil? ? 0 : turn-1 - store[lastNumber]
+      store[lastNumber] = turn - 1
+      lastNumber = diff
+    end
+  end
+  return lastNumber
+end
+
+def part2 (input)
+
+end
+
+if caller.length == 0
+  puts "Solution for part1: %d" % part1([1,2,16,19,18,0])
+  #puts "Solution for part2: %d" % part2(input[1])
+end
