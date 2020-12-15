ca71ea09ea7c8bf1339241d7bbb0a8883303f159
mschlund <3297755+mschlund@users.noreply.github.com>
2020-12-15 16:16:06 +0000

    Day12 solution.

--- day12/python/mschlund/solution.py
+++ day12/python/mschlund/solution.py
@@ -32,10 +32,44 @@ def execute_movement_1(ship_pos, direction, line):
     new_dir = int((direction + num/90)) % 4
   return new_pos, new_dir
 
-  def execute_movement_2(pos, stride, direction, line):
-    cmd = line[0]
-    num = int(line[1:])
-    
+
+def pos_move(pos, stride, num):
+  new_pos = (pos[0] + stride[0]*num,
+             pos[1] + stride[1]*num)
+  return new_pos
+
+def turn_left(stride):
+  return (-stride[1], stride[0])
+
+def turn_right(stride):
+  return (stride[1], -stride[0])
+
+
+def move_stride(stride, cmd, num):
+  return pos_shift(stride, DIRS.index(cmd), num)
+
+def execute_movement_2(pos, stride, line):
+  cmd = line[0]
+  num = int(line[1:])
+  if cmd == 'F':
+    new_pos = pos_move(pos, stride, num)
+    new_stride = stride
+  elif cmd in ['N', 'S', 'E', 'W']:
+    new_pos = pos
+    new_stride = move_stride(stride, cmd, num)
+  elif cmd == 'L':
+    new_pos = pos
+    times = int(num/90)
+    for i in range(times):
+      new_stride = turn_left(stride)
+      stride = new_stride
+  elif cmd == 'R':
+    new_pos = pos
+    times = int(num/90)
+    for i in range(times):
+      new_stride = turn_right(stride)
+      stride = new_stride
+  return new_pos, new_stride
 
 def main():
   input_filename = 'input.txt'
@@ -48,11 +82,16 @@ def main():
       print('Pos: {}, Dir: {}'.format(ship_pos, DIRS[direction]))
       dist_to_origin = abs(ship_pos[0]) + abs(ship_pos[1])
       print('Distance to origin = {}'.format(dist_to_origin))
+    print('-------------------------------')
     # star 2:
     stride = (10, 1)
+    pos = (0,0)
     for m in movements:
-      pos, stride, direction = execute_movement_2(pos, stride, direction, m)
-
+      pos, stride = execute_movement_2(pos, stride, m)
+      print('Pos: {}, Stride: {}'.format(pos,stride))
+      dist_to_origin = abs(pos[0]) + abs(pos[1])
+      print('Distance to origin = {}'.format(dist_to_origin))
+    
 
 if __name__ == "__main__":
   main()
