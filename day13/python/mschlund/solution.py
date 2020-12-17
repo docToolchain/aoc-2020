ecca2451c0dee07f70bd376e7b8072972bc45bb4
mschlund <3297755+mschlund@users.noreply.github.com>
2020-12-17 13:36:26 +0000

    Star1 - day13

---
+++ day13/python/mschlund/solution.py
@@ -0,0 +1,18 @@
+def get_next_bus(t, busses):
+  times_to_wait = [(-t % x, x) for x in busses]
+  return min(times_to_wait)
+
+def main():
+  input_filename = 'input.txt'
+  with open(input_filename, 'r') as input:
+    whole_input = input.read().split('\n')
+    time = int(whole_input[0])
+    bus_ids = [int(x) for x in whole_input[1].split(',') if x != 'x']
+    t, bus_id = get_next_bus(time, bus_ids)
+    print((t,bus_id))
+    print('Star1: {}'.format(t*bus_id))
+    
+
+
+if __name__ == "__main__":
+  main()
