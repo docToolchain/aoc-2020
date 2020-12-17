e417476b90f957bcf08caba28420e9c64a228905
mschlund <3297755+mschlund@users.noreply.github.com>
2020-12-17 12:42:24 +0000

    Solution for day16 complete.

--- day16/python/mschlund/solution.py
+++ day16/python/mschlund/solution.py
@@ -1,3 +1,8 @@
+from collections import defaultdict
+import math
+import networkx as nx
+import networkx.algorithms.bipartite
+
 def parse_field_and_ranges(section):
   constraints_string = section.split('\n')
   ranges = {}
@@ -16,15 +21,36 @@ def check_other_tickets(section, ranges):
   valid_tickets = []
   for line in lines:
     numbers = line.split(',')
+    ticket_erroneous = False
     for n in numbers:
       num_int = int(n)
       flattened_vals = [x for l in ranges.values() for x in l]
       res = [m <= num_int and num_int <= M for (m,M) in flattened_vals]
       if not any(res):
         error_rate += num_int
-      else:
-        valid_tickets.append(line)
+        ticket_erroneous = True
+    if not ticket_erroneous:
+      valid_tickets.append(line)
+
   return error_rate, valid_tickets
+
+
+def get_field_positions(valid_tickets, ranges):
+  field_positions = defaultdict(list)
+  field_to_pos = {f : set(range(valid_tickets[0].count(',')+1)) for f in ranges.keys()}
+
+  for ticket in valid_tickets:
+    numbers = map(int, ticket.split(','))
+    for i,n in enumerate(numbers):
+      for field_name in ranges.keys():
+        (m1, M1), (m2, M2) = ranges[field_name]
+        if (m1 <= n and n <= M1) or (m2 <= n and n <= M2):
+          field_positions[field_name].append(i)
+        else:
+          if i in field_to_pos[field_name]:
+            #print('{} cannot be {}, {} not in range'.format(i, field_name, n))
+            field_to_pos[field_name].remove(i)
+  return field_to_pos
 
 def main():
   input_filename = 'input.txt'
@@ -35,6 +61,17 @@ def main():
     error_rate, valid_tickets = check_other_tickets(input_split[2], ranges)
     # star 1:
     print('Error rate: {}'.format(error_rate))
+    # star 2:
+    positions = get_field_positions(valid_tickets, ranges)
+    G = nx.Graph()
+    for f in positions.keys():
+      for v in positions[f]:
+        G.add_edge(f, v)
+    matching = nx.algorithms.bipartite.hopcroft_karp_matching(G)
+    departure_indices = [matching[x] for x in matching.keys() if type(x)==str and x.startswith('departure')]
+    print(departure_indices)
+    my_ticket = [int(x) for x in input_split[1].split('\n')[1].split(',')]
+    print('Product of all departure-fields: {}'.format(math.prod([my_ticket[i] for i in departure_indices])))
 
 if __name__ == "__main__":
   main()
