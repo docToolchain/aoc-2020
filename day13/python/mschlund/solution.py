610f06909396acb092328e5bc72b7e24b490a68c
mschlund <3297755+mschlund@users.noreply.github.com>
2020-12-18 16:08:35 +0000

    Solution day13 + Readme.

--- day13/python/mschlund/solution.py
+++ day13/python/mschlund/solution.py
@@ -1,7 +1,13 @@
+import sympy
+import sympy.ntheory.modular as mod
+
+# tag::starOne[]
 def get_next_bus(t, busses):
   times_to_wait = [(-t % x, x) for x in busses]
   return min(times_to_wait)
+# end::star_1[]
 
+# tag::main[]
 def main():
   input_filename = 'input.txt'
   with open(input_filename, 'r') as input:
@@ -11,7 +17,13 @@ def main():
     t, bus_id = get_next_bus(time, bus_ids)
     print((t,bus_id))
     print('Star1: {}'.format(t*bus_id))
-    
+    print('-------------------')
+    zipped_ids = [(int(x),-i) for i,x in enumerate(whole_input[1].split(',')) if x != 'x']
+    m,v = zip(*zipped_ids)
+    res = mod.crt(m, v)
+    print("Star 2: {}".format(res))
+# end::main[]
+
 
 
 if __name__ == "__main__":
