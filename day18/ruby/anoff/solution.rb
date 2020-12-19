f55cd5604d55e3900d6f305bf8ebba7272133016
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2020-12-19 11:09:10 +0900

    day18/2

--- day18/ruby/anoff/solution.rb
+++ day18/ruby/anoff/solution.rb
@@ -50,8 +50,57 @@ def calc(input, stopAtParenthesesClose=false)
   end
   return result
 end
+
+def calc2(input)
+  eq = input.gsub(/\s/, "") # remove all spaces
+  while eq.to_i.to_s != eq
+    p eq
+    # first solve all additions that do not involve parenthesis
+    eq = eq.gsub(/(\d+)\+(\d+)/) { |match|
+      a, b = match.split("+")
+      a.to_i + b.to_i
+    }
+    # remove useless parentheses
+    eq = eq.gsub(/\((\d+)\)/, '\1')
+    # multiply only within parentheses
+    eq = eq.gsub(/\((\d+)\*(\d+)\)/) { |match|
+      a, b = match.split("*")
+      a[1..-1].to_i * b[0..-2].to_i
+    }
+    # remove useless parentheses
+    eq = eq.gsub(/\((\d+)\)/, '\1')
+    # handle parenthesis recursively
+    eq = eq.gsub(/\([\d\+\*]+\)/) { |match|
+      subeq = match[1..-2] # remove parentheses
+      calc2(subeq)
+    }
+    # remove useless parentheses
+    eq = eq.gsub(/\((\d+)\)/, '\1')
+    # only after all + and () have been solved start multiplying stuff
+    if not (eq.include?("+") || eq.include?("("))
+      eq = eq.gsub(/(\d+)\*(\d+)/) { |match|
+        a, b = match.split("*")
+        a.to_i * b.to_i
+      }
+    end
+
+    # remove useless parentheses
+    eq = eq.gsub(/\((\d+)\)/, '\1')
+  end
+  return eq.to_i
+end
+
+def part2 (input)
+  sum = 0
+  for line in input
+    result = calc2(line)
+    sum += result
+  end
+  return sum
+end
+
 if __FILE__ == $PROGRAM_NAME
   input = readInput("./input.txt")
   puts "Solution for part1: %d" % part1(input)
-  #puts "Solution for part2: #{part2(input)}"
+  puts "Solution for part2: #{part2(input)}"
 end
--- day18/ruby/anoff/solution_test.rb
+++ day18/ruby/anoff/solution_test.rb
@@ -9,4 +9,12 @@ class TestSolution < Test::Unit::TestCase
     assert_equal 12240, calc("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")
     assert_equal 13632, calc("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
   end
+
+  def test_calc2
+    assert_equal 51, calc2("1 + (2 * 3) + (4 * (5 + 6))")
+    assert_equal 46, calc2("2 * 3 + (4 * 5)")
+    assert_equal 1445, calc2("5 + (8 * 3 + 9 + 3 * 4 * 3)")
+    assert_equal 669060, calc2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")
+    assert_equal 23340, calc2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
+  end
 end
