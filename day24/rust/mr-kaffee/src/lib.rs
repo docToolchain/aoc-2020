37255e37ed3ab637e7bb2df5dc40059d850df264
Peter Wieland <peter@die-wielands.net>
2020-12-24 09:54:36 +0100

    Optimize solution for day24/rust/mr-kaffee
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day24/rust/mr-kaffee/src/lib.rs
+++ day24/rust/mr-kaffee/src/lib.rs
@@ -39,21 +39,29 @@ pub fn update_step(blacks: &HashSet<(i32, i32)>) -> HashSet<(i32, i32)> {
                   (min(min_x, *x), max(max_x, *x + 1),
                    min(min_y, *y), max(max_y, *y + 1)));
 
+    // closure to check whether tile at coordinate is black
+    // check whether it is in range first
+    let is_black = |(x, y)| x >= min_x && x < max_x && y >= min_y && y < max_x &&
+        blacks.contains(&(x, y));
+
+    // valid coordinates: even row -> even column, odd row -> odd column
+    // in even rows, start from largest even x which is less than min_x
+    // in odd rows, start from largest odd xx which is less then min_x
+    let d = if min_x & 1 == 0 { [-2, -1] } else { [-1, -2] };
+
     let mut blacks_upd = HashSet::new();
 
     // search directions
     let dlts = [(2, 0), (1, 1), (1, -1), (-2, 0), (-1, -1), (-1, 1)];
 
-    // valid coordinates: even row -> even column, odd row -> odd column
-    // for invalid coordinate, tile will always be white with zero neighbors.
     for y in min_y - 1..max_y + 1 {
-        for x in min_x - 1..max_x + 1 {
+        for x in (min_x + d[(y & 1) as usize]..max_x + 1).step_by(2) {
             // is currently black
-            let black = blacks.contains(&(x, y));
+            let black = is_black((x, y));
 
             // count black adjacents
             let cnt = dlts.iter()
-                .filter(|&(dx, dy)| blacks.contains(&(x + dx, y + dy)))
+                .filter(|&(dx, dy)| is_black((x + dx, y + dy)))
                 .count();
 
             // apply update rules
